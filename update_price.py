8348d01741952ccde8ba8955c8c7d5535934701b
thangtm58 <thangtm589@gmail.com>
2025-05-12 10:34:44 +0000

    update code to update price

--- update_price.py
+++ update_price.py
@@ -1,6 +1,8 @@
 import requests
 import time
 from vnstock import Vnstock
+from datetime import timedelta
+import pandas as pd
 
 # Set symbol
 symbol = 'VCB'
@@ -25,12 +27,20 @@ def run_bot():
             # Try to fetch intraday data
             stk = stock.quote.intraday(symbol=symbol, to_df=True)
 
+            # If data is too old -> raise error -> will be caught below
+            stk['time'] = pd.to_datetime(stk['time'])  # ensure datetime format
+            last_time = stk['time'].iloc[-1]
+            now = pd.Timestamp.now()
+
+            if (now - last_time) > timedelta(minutes=10):
+                raise ValueError("Data is too old (>10 minutes).")
+
             # If no data or dataframe is empty -> raise error -> will be caught below
             if stk is None or stk.empty:
                 raise ValueError("No data available right now.")
 
-            last_buy = stk[stk['match_type'] == 'Buy'].tail(1)
-            last_sell = stk[stk['match_type'] == 'Sell'].tail(1)
+            last_buy = stk[stk['match_type'] == 'Buy'].iloc[-1]
+            last_sell = stk[stk['match_type'] == 'Sell'].iloc[-1]
 
             # If no buy or sell transactions -> raise error -> will be caught below
             if last_buy.empty or last_sell.empty:
@@ -38,9 +48,11 @@ def run_bot():
 
             last_buy_price = float(last_buy['price'].iloc[0])
             last_sell_price = float(last_sell['price'].iloc[0])
+            last_buy_vol = float(last_buy['volume'].iloc[0])
+            last_sell_vol = float(last_sell['volume'].iloc[0])
 
             send_telegram_message(
-                f"{symbol} Buy: {last_buy_price} Sell: {last_sell_price}",
+                f"Time: {last_time}:\n{symbol} Buy: {last_buy_price}, Vol: {last_buy_vol}\nSell: {last_sell_price}, Vol: {last_sell_vol}",
                 BOT_TOKEN,
                 CHAT_ID
             )
@@ -50,6 +62,6 @@ def run_bot():
             print(f"Error or No Data: {e}")
 
         # Always sleep 600 seconds after each run (success or error), then retry
-
+        time.sleep(600)
 if __name__ == "__main__":
     run_bot()
